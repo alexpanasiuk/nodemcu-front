7c98a19ba71d45c320de0c85c29769119507b736
alex <alex.panasiuk99@gmail.com>
2020-03-03 15:28:12 +0200

    build

---
+++ build/precache-manifest.1145b992a62678e91595de3bfc7f3052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dc5a9ae4c4a0b4eb43407b46ab8167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "100e8363ad9ba3b9f95c",
+    "url": "/static/css/main.8bace027.chunk.css"
+  },
+  {
+    "revision": "786fbbe75059189d5bf3",
+    "url": "/static/js/2.73e8e2e3.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.73e8e2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100e8363ad9ba3b9f95c",
+    "url": "/static/js/main.3815285e.chunk.js"
+  },
+  {
+    "revision": "d06f0e15d0d99e70ca99",
+    "url": "/static/js/runtime-main.d4cdbfe6.js"
+  }
+]);
